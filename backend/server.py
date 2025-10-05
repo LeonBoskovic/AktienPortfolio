377850d615a942a500ce5cdaa1d7f4131a0ef176
LeonBoskovic <leon.boskovic@edu.gbssg.ch>
2025-10-06 00:14:47 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,7 @@ from pymongo import MongoClient
 
 ROOT_DIR = Path(__file__).parent
 if os.getenv("RENDER") is None:
-load_dotenv(ROOT_DIR / '.env')
+    load_dotenv(ROOT_DIR / '.env')
 
 # MongoDB connection
 mongo_url = os.environ['MONGO_URL']
