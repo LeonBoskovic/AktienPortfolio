41143ddf52b2c27a68024f7365db4f28feaad5c4
LeonBoskovic <leon.boskovic@edu.gbssg.ch>
2025-10-06 13:18:59 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, APIRouter, HTTPException, Depends, Request, Response
-from fastapi.responses import RedirectResponse
+from fastapi.responses import JSONResponse
 from fastapi.security import HTTPBearer
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
@@ -238,8 +238,8 @@ async def register(user_data: UserCreate):
     user_dict['hashed_password'] = hashed_password
     await db.users.insert_one(user_dict)
 
-    return RedirectResponse(url="/login",
-  status_code=303)
+    return JSONResponse({"success": True, "message": "User
+registered successfully"})
 
 
 
